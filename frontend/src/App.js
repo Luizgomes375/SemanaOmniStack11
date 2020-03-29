5aec8fee720725305aeedce87268da23d9c7b04a
luiz Henrique <luizgomes375@gmail.com>
2020-03-29 01:30:32 -0300

    React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import './global.css'
 
 import Logon from './pages/logon'
-
-import Routes from './routes'
+import Register from './pages/register'
+import Profile from './pages/profile'
 
 function App() {
   
   return (
-    <Routes/>
+    <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Logon}/>
+      <Route path="/register" component={Register}/>
+      <Route path="/profile" component={Profile}/>
+    </Switch>
+    </BrowserRouter>
    
   )
 }
